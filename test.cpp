57f79975a827a427a5538deed2ccf7e589e9a120
Rachel Lam <rachel.lam@colorado.edu>
2023-01-26 13:46:20 -0700

    removed one test case

--- test.cpp
+++ test.cpp
@@ -3,12 +3,12 @@
 #include "functions_to_implement.cpp"
 #include "catch.hpp"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
+// TEST_CASE( "Factorials are computed", "[factorial]" ) {
+//     REQUIRE( Factorial(1) == 1 );
+//     REQUIRE( Factorial(2) == 2 );
+//     REQUIRE( Factorial(3) == 6 );
+//     REQUIRE( Factorial(10) == 3628800 );
+// }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(0) == 1 );
